6a8353d76176668a841675263c9d4f9e8d0b3133
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-14 13:54:53 +0900

    require & deploy mysql

--- deploy.rb
+++ deploy.rb
@@ -5,5 +5,8 @@ Dotenv.load
 
 arukas = Arukas::API::new(ENV["ARUKAS_JSON_API_TOKEN"], ENV["ARUKAS_JSON_API_SECRET"])
 
+json = File.read("mysql.json")
+arukas.create_apps(json)
+
 json = File.read("wp.json")
 arukas.create_apps(json)
