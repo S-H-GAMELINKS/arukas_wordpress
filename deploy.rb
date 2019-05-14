2200e530d6f121726f0256173ae138aeb7fde799
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-14 13:49:18 +0900

    loading wp.json

--- deploy.rb
+++ deploy.rb
@@ -5,5 +5,5 @@ Dotenv.load
 
 arukas = Arukas::API::new(ENV["ARUKAS_JSON_API_TOKEN"], ENV["ARUKAS_JSON_API_SECRET"])
 
-json = File.read("test.json")
+json = File.read("wp.json")
 arukas.create_apps(json)
